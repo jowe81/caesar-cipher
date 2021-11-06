33035bbdb46b8d4b37aa3c1a290589c72b87b43e
Johannes Weber <johannes@drweber.de>
2021-11-06 13:34:11 -0700

    Cleanup

--- caesar.js
+++ caesar.js
@@ -1,56 +1,40 @@
 const getCipher = (char, key) => {
-  /*
-
-    lowerCaseA = 65
-
-  */
   let lowerCaseA = 65;
   let lowerCaseZ = 90;
   let upperCaseA = 97;
   let upperCaseZ = 122;
 
   const x = char.charCodeAt(0);
-  let newCode = x; //Default to no shift
+  let newCode = x; //Default to no shift (non alpha characters won't be touched)
+
+  key = key % 26; //Ensure validity of key
 
   if (x >= lowerCaseA && x <= lowerCaseZ || x >= upperCaseA && x <= upperCaseZ) {
     //alpha
-    newCode = x - key;
+    newCode = x - key; //Shift first, deal with wrapping after
     if (key > 0) {
       //Positive key -> left-shift
-      if (x >= lowerCaseA + key && x <= lowerCaseZ || x >= upperCaseA + key && x <= upperCaseZ) {
-        //character doesn't need to wrap, just transpose
-        newCode = x - key;
-      } else {
-        //character needs to wrap
-        newCode = x - key + 26;
+      if (!(x >= lowerCaseA + key && x <= lowerCaseZ || x >= upperCaseA + key && x <= upperCaseZ)) {
+        newCode += 26; //Wrap to right
       }
     } else {
       //Negative key -> right-shift
-      if (x >= lowerCaseA && x <= lowerCaseZ + key || x >= upperCaseA && x <= upperCaseZ + key) {
-        //character doesn't need to wrap, just transpose
-        newCode = x - key;
-      } else {
-        //character needs to wrap
-        newCode = x - key - 26;
+      if (!(x >= lowerCaseA && x <= lowerCaseZ + key || x >= upperCaseA && x <= upperCaseZ + key)) {
+        newCode -= 26; //Wrap to left
       }
-      
     }
   }
-
 
   return String.fromCharCode(newCode);
 };
 
 const encrypt = (plaintext, key) => {
   /*
-    for each character in plaintext
-      return new ascii code
-
-    ASCII char codes:
-      uppercase 65 - 90
-      lowercase 97 - 122
-
-    */
+    split string into array of characters
+    for each character in array
+      shift this character and add it to the output array
+    return the string resulting from joining the output array
+  */
   const output = [];
   plaintext.split('').forEach(element => {
     output.push(getCipher(element, -key));
